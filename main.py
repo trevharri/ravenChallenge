d9fd2bfbae474ddf45ba0216794a8ab8fd7cf28e
trevharri <trevor.harri94@gmail.com>
2024-08-29 18:49:22 +0200

    added basic validation for rp_entity_id format

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-import json
+import json, re
 from collections import defaultdict
 TEST_PATH = "/Users/trevorharrington/Documents/ravenChallenge/rt-feed-record"
 
 
 def process_json_lines(file_path):
     lines_by_document_id = defaultdict(list)
-    unique_ids = set()
 
     with open(file_path, "r") as f:
             data = f.readlines()
             for line in data:
                 line = json.loads(line.strip())
                 lines_by_document_id[line["RP_DOCUMENT_ID"]].append(line)
-                line_id = line["RP_DOCUMENT_ID"].encode('ascii')
 
     return lines_by_document_id
 
@@ -36,10 +34,20 @@ def check_for_missing_records(lines_by_document_id):
     if len(missing_data) > 0:
         return missing_data
     else:
-        return false
+        return False
+
+def validate_rp_entity_id(id_string):
+    id_string = id_string
+    if not isinstance(id_string, unicode):
+        return False
+
+    pattern = r'^[A-Z0-9]{6}$'
+
+    return bool(re.match(pattern, id_string))
 
 processed_data = process_json_lines(TEST_PATH)
 missing_data = check_for_missing_records(processed_data)
 
 print(len(set(processed_data.keys())))
 print(missing_data)
+print(validate_rp_entity_id(u"46Ee13"))
